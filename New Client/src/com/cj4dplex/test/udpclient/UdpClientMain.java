28bcc307dfd51f989187cd1ca483cdb70e7d95f4
hisouske <zzang22yn@naver.com>
2021-05-31 22:03:21 +0900

    fix package Name

--- New Client/src/com/cj4dplex/test/udpclient/UdpClientMain.java
+++ New Client/src/com/cj4dplex/test/udpclient/UdpClientMain.java
@@ -5,6 +5,7 @@ import com.cj4dplex.test.udpcontroller.Controller;
 public class UdpClientMain {
 
 	public static void main(String[] args) {
+		//udpcontroller, udpserver 패키지 대문자 변경확인 20210531
 		UdpClient udpclient = new UdpClient("localhost", 6000);
 		Controller controller = new Controller(udpclient);
 	}
